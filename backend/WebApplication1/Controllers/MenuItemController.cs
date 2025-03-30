e0e4110de2a2c83b925c02f1c7264fc7facc40a3
Ngchphong <nguyenchanphongmt@gmail.com>
2025-03-30 22:54:29 +0700

    Tui lam CRUD cho Menu Item roi a

---
+++ backend/WebApplication1/Controllers/MenuItemController.cs
@@ -0,0 +1,79 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Database;
+
+namespace WebApplication1.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class MenuItemController : ControllerBase
+{
+    private readonly ApplicationDbContext _context;
+
+    public MenuItemController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+    
+    //[GET] lấy tất cả menu items
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<MenuItem>>> GetMenuItems()
+    {
+        return await _context.MenuItemsTable.ToListAsync();
+    }
+
+    //lấy theo số id
+    [HttpGet("{id}")]
+    public async Task<ActionResult<MenuItem>> GetMenuItemById(int id)
+    {
+        var menuItem = await _context.MenuItemsTable.FindAsync(id);
+
+        if (menuItem == null)
+        {
+            return NotFound(new { message = "Không tìm thấy menu item" });
+        }
+        return menuItem;
+    }
+    
+    //Thêm menu item mới
+    [HttpPost]
+    public async Task<ActionResult<MenuItem>> AddMenuItem(MenuItem newMenuItem)
+    {
+        _context.MenuItemsTable.Add(newMenuItem);
+        await _context.SaveChangesAsync();
+        return CreatedAtAction(nameof(GetMenuItemById), new { id = newMenuItem.ItemId }, newMenuItem);
+    }
+
+    //Cập nhật menu item
+    [HttpPut("{id}")]
+    public async Task<ActionResult> UpdateMenuItem(int id, MenuItem menuItem_Update)
+    {
+        var findId = await _context.MenuItemsTable.FindAsync(id);
+        if (findId == null)
+        {
+            return NotFound(new { message = "Không tìm thấy menu item" });
+        }
+        
+        findId.ItemName = menuItem_Update.ItemName;
+        findId.Description = menuItem_Update.Description;
+        findId.BasePrice = menuItem_Update.BasePrice;
+        findId.IsActive = menuItem_Update.IsActive;
+        findId.CreatedAt = menuItem_Update.CreatedAt;
+        
+        await _context.SaveChangesAsync();
+        return Ok(new { message = "Hoàn thành cập nhật" });
+    }
+    
+    //Xóa menu item
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> DeleteMenuItem(int id)
+    {
+        var delete = await _context.MenuItemsTable.FindAsync(id);
+        if (delete == null)
+        {
+            return NotFound(new { message = "Không tìm thấy id" });
+        }
+        _context.MenuItemsTable.Remove(delete);
+        await _context.SaveChangesAsync();
+        return Ok(new { message = "Xóa thành công" });
+    }
+}
