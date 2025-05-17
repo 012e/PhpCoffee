12eda074f823cce49670b963a17fa43dbb96727c
012e <huyphmnhat@gmail.com>
2025-05-17 15:32:21 +0700

    refactor order controller

--- backend/WebApplication1/Controllers/OrderController.cs
+++ backend/WebApplication1/Controllers/OrderController.cs
@@ -1,76 +1,69 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Globalization;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using WebApplication1.Database;
 using WebApplication1.Dtos.Mappers;
 using WebApplication1.Dtos.Requests.Orders;
-using WebApplication1.Dtos.Requests.Sepay;
 
-namespace WebApplication1.Controllers
+namespace WebApplication1.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class OrderController : Controller
 {
-    [Route("[controller]")]
-    [ApiController]
-    public class OrderController : Controller
+    private readonly ApplicationDbContext _context;
+    private readonly OrdersMapper _ordersMapper;
+
+    public OrderController(ApplicationDbContext context, OrdersMapper ordersMapper)
     {
-        private readonly ApplicationDbContext _context;
-        private readonly OrdersMapper _ordersMapper;
+        _ordersMapper = ordersMapper;
+        _context = context;
+    }
 
-        public OrderController(ApplicationDbContext context, OrdersMapper ordersMapper)
+    [HttpPost]
+    [ProducesResponseType(typeof(Order), StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<Order>> Create(CreateOrdersRequest createOrdersRequest)
+    {
+        await using var transaction = await _context.Database.BeginTransactionAsync();
+        try
         {
-            _ordersMapper = ordersMapper;
-            _context = context;
+            var newOrder = _ordersMapper.CreateOrdersRequestToOrder(createOrdersRequest);
+            _context.Orders.Add(newOrder);
+            await _context.SaveChangesAsync();
+            foreach (var item in createOrdersRequest.CreateOrderItemRequests)
+            {
+                var newItem = _ordersMapper.CreateOrderItemRequestInOrders(item);
+                newItem.TotalPrice = newItem.UnitPrice * newItem.Quantity;
+                newItem.OrderId = newOrder.OrderId;
+                _context.OrderItems.Add(newItem);
+                newOrder.TotalAmount += newItem.TotalPrice;
+            }
+
+            newOrder.Remaining = newOrder.TotalAmount;
+            _context.Orders.Update(newOrder);
+            await _context.SaveChangesAsync();
+
+            var newPayment = _ordersMapper.CreatePaymentRequestInOrders(createOrdersRequest.CreatePaymentRequest);
+            newPayment.OrderId = newOrder.OrderId;
+            newPayment.Amount = newOrder.Remaining;
+
+            _context.Payments.Add(newPayment);
+
+            newOrder.PaymentStatus = newPayment.PaymentMethod switch
+            {
+                "Tien mat" => "Da thanh toan",
+                "Chuyen khoan" => "Doi thanh toan",
+                _ => newOrder.PaymentStatus
+            };
+
+            _context.Update(newOrder);
+            await _context.SaveChangesAsync();
+            await transaction.CommitAsync();
+            return Ok();
         }
-
-        [HttpPost]
-        [ProducesResponseType(typeof(Order), StatusCodes.Status201Created)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public async Task<ActionResult<Order>> Create(CreateOrdersRequest createOrdersRequest)
+        catch (Exception ex)
         {
-            using var transaction = await _context.Database.BeginTransactionAsync();
-            try
-            {
-                var newOrder = _ordersMapper.CreateOrdersRequestToOrder(createOrdersRequest);
-                _context.Orders.Add(newOrder);
-                await _context.SaveChangesAsync();
-                foreach (var item in createOrdersRequest.CreateOrderItemRequests)
-                {
-                    var newItem = _ordersMapper.CreateOrderItemRequestInOrders(item);
-                    newItem.TotalPrice = newItem.UnitPrice * newItem.Quantity;
-                    newItem.OrderId = newOrder.OrderId;
-                    _context.OrderItems.Add(newItem);
-                    newOrder.TotalAmount += newItem.TotalPrice;
-                }
-                newOrder.Remaining = newOrder.TotalAmount;
-                _context.Orders.Update(newOrder);
-                await _context.SaveChangesAsync();
-                var newPayment = _ordersMapper.CreatePaymentRequestInOrders(createOrdersRequest.CreatePaymentRequest);
-                newPayment.OrderId = newOrder.OrderId;
-                newPayment.Amount = newOrder.Remaining;
-                _context.Payments.Add(newPayment);
-                if (newPayment.PaymentMethod == "Tien mat")
-                {
-                    newOrder.PaymentStatus = "Da thanh toan";
-                }
-                else if (newPayment.PaymentMethod == "Chuyen khoan")
-                {
-                    newOrder.PaymentStatus = "Doi thanh toan";
-                }
-                _context.Update(newOrder);
-                await _context.SaveChangesAsync();
-                await transaction.CommitAsync();
-                return Ok();
-            }
-            catch (Exception ex)
-            {
-                await transaction.RollbackAsync();
-                return BadRequest("Failed to create orders: " + ex.Message);
-            }
+            await transaction.RollbackAsync();
+            return BadRequest("Failed to create orders: " + ex.Message);
         }
     }
 }
