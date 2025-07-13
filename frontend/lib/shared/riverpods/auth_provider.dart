29b6e778830419fe79ba999b35d9f5472d2a0d9e
Pham Nhat Huy <huyphmnhat@gmail.com>
2025-07-13 15:51:10 +0700

    feat: federated logout

--- frontend/lib/shared/riverpods/auth_provider.dart
+++ frontend/lib/shared/riverpods/auth_provider.dart
@@ -1,6 +1,7 @@
 import 'package:frontend/shared/services/token_service.dart';
 import 'package:get_it/get_it.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 part 'auth_provider.g.dart';
 
@@ -17,13 +18,49 @@ class AuthNotifier extends _$AuthNotifier {
     return state != null;
   }
 
-  login({required String accessToken}) async {
+  login({required String accessToken, String? idToken}) async {
     await _tokenService.setToken(accessToken);
+    if (idToken != null) {
+      await _tokenService.setIdToken(idToken);
+    }
     state = accessToken;
   }
 
-  void logout() async {
+  Future<void> logout({String? postLogoutRedirectUri, bool federated = false}) async {
+    final idToken = await _tokenService.getIdToken();
+    
+    // Perform federated logout with Auth0
+    if (idToken != null) {
+      await _performFederatedLogout(idToken, postLogoutRedirectUri, federated);
+    }
+    
+    // Clear local tokens
     await _tokenService.removetoken();
     state = null;
+  }
+
+  Future<void> _performFederatedLogout(String idToken, String? postLogoutRedirectUri, bool federated) async {
+    const auth0Domain = "dev-z88j6uoisbogqn82.us.auth0.com";
+    
+    // Build OIDC logout URL according to Auth0 documentation
+    final queryParams = <String, String>{
+      'id_token_hint': idToken,
+    };
+    
+    if (postLogoutRedirectUri != null) {
+      queryParams['post_logout_redirect_uri'] = postLogoutRedirectUri;
+    }
+    
+    // Add federated parameter to log user out of identity provider
+    if (federated) {
+      queryParams['federated'] = '';
+    }
+    
+    final logoutUrl = Uri.https(auth0Domain, '/oidc/logout', queryParams);
+
+    // Open logout URL in external browser
+    if (await canLaunchUrl(logoutUrl)) {
+      await launchUrl(logoutUrl, mode: LaunchMode.externalApplication);
+    }
   }
 }
